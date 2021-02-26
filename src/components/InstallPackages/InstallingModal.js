adfa5d8b3bfe5f08b36852f6248ed16e110d0651
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2021-02-27 02:17:53 +0530

    Tweaked UI of install status page

--- src/components/InstallPackages/InstallingModal.js
+++ src/components/InstallPackages/InstallingModal.js
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from 'react';
 
 import {
-	AppBar,
-	Avatar,
-	Box,
-	Dialog,
-	Grid,
-	IconButton,
-	List,
-	ListItem,
-	ListItemAvatar,
-	ListItemText,
-	makeStyles,
-	Paper,
-	Toolbar,
-	Typography,
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  CircularProgress,
+  Dialog,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  makeStyles,
+  Toolbar,
+  Typography,
 } from '@material-ui/core';
-import { GetApp } from '@material-ui/icons';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import { green, red } from '@material-ui/core/colors';
+import { Cancel, CheckOutlined, GetApp } from '@material-ui/icons';
 
 import { SlideDialogTransition } from '../../commons/helpers';
 
@@ -35,13 +36,16 @@ const useStyles = makeStyles(theme => ({
 		background: '#1e1e1e',
 		padding: theme.spacing(2),
 		fontSize: 16,
+		overflowY: 'scroll',
 	},
 }));
 
 export default function InstallPackagesStatus({ isOpen, handleClose }) {
 	const classes = useStyles();
-	const [installOutput, setInstallOutput] = useState('');
+	const [installOutput, setInstallOutput] = useState(null);
 	const [packageNameMessage, setPackageNameMessage] = useState({});
+
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		ipcRenderer.on('INSTALL_OUTPUT', function (ev, installOutput) {
@@ -50,13 +54,34 @@ export default function InstallPackagesStatus({ isOpen, handleClose }) {
 
 		ipcRenderer.on(
 			'PACKAGE_STATUS_AFTER_INSTALL',
-			function (ev, packageName, packageMessage,error) {
-				let _packageNameMessage = { ...packageNameMessage };
-				_packageNameMessage[packageName] = {packageMessage, error};
+			function (ev, packageName, packageMessage, error) {
+				let _packageNameMessage = { ...packageNameMessage};
+				_packageNameMessage[packageName] = { packageMessage, error };
+				console.log(packageName, packageMessage, error);
 				setPackageNameMessage(_packageNameMessage);
 			},
 		);
 	}, []);
+
+	useEffect(() => {
+		if (
+			installOutput !== null &&
+			Object.keys(packageNameMessage).length === 0
+		) {
+			setLoading(false);
+		}
+	}, [installOutput, packageNameMessage]);
+
+	const loader = (
+		<Grid
+			container
+			justify="center"
+			alignContent="center"
+			style={{ height: '100vh',  }}
+		>
+			<CircularProgress size={40} />
+		</Grid>
+	);
 
 	return (
 		<div>
@@ -78,39 +103,76 @@ export default function InstallPackagesStatus({ isOpen, handleClose }) {
 						<Typography variant="h6" className={classes.title}>
 							Installing Packages
 						</Typography>
+
+						<Button autoFocus variant="contained" color="secondary">
+							Done
+						</Button>
 					</Toolbar>
 				</AppBar>
 
-				<Grid container direction="column">
-					<Grid item>
-						<Box height="50vh">
-							<Paper elevation={5}>
-								<List className={classes.root}>
-									{Object.entries(packageNameMessage).map(([packageName,packageMessage]) => (
-										
-									<ListItem>
-										<ListItemAvatar>
-											<Avatar>
-												<CheckCircleOutlineIcon />
-											</Avatar>
-										</ListItemAvatar>
-										<ListItemText
-											primary={packageName}
-											secondary={packageMessage}
-										/>
-									</ListItem>
-									))}
-									
-								</List>
-							</Paper>
-						</Box>
-					</Grid>
-					<Grid item xs={12}>
-						<Box className={classes.pre} height="50vh" width="100%">
-							<pre>{installOutput}</pre>
-						</Box>
-					</Grid>
-				</Grid>
+				{loading ? (
+					loader
+				) : (
+					<div>
+						<Grid container>
+							<Grid item xs={2} />
+							<Grid item xs={8}>
+								<Box height="48vh">
+									<List>
+										{Object.entries(packageNameMessage).map(
+											([
+												packageName,
+												packageInstallData,
+											], index) => (
+												<ListItem divider key={`install-status-list-item-${index}`}>
+													<ListItemAvatar>
+														<Avatar
+															style={{
+																backgroundColor: packageInstallData.error
+																	? red[300]
+																	: green[300],
+															}}
+														>
+															{packageInstallData.error ? (
+																<Cancel />
+															) : (
+																<CheckOutlined />
+															)}
+														</Avatar>
+													</ListItemAvatar>
+													<ListItemText
+														primary={packageName}
+														secondary={
+															packageInstallData.packageMessage
+														}
+													/>
+												</ListItem>
+											),
+										)}
+									</List>
+								</Box>
+							</Grid>
+						</Grid>
+						<Grid container>
+							<Grid item xs={2} />
+							<Grid item xs={8}>
+								<Typography variant="subtitle">
+									Output:
+								</Typography>
+								<Box className={classes.pre} height="48vh">
+									<pre
+										style={{
+											whiteSpace: 'break-spaces',
+											wordBreak: 'break-word',
+										}}
+									>
+										{installOutput}
+									</pre>
+								</Box>
+							</Grid>
+						</Grid>
+					</div>
+				)}
 			</Dialog>
 		</div>
 	);
