ebfded459795231b93f0b9550e2e737a73229027
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2021-03-26 07:36:10 +0530

    Added react hook form to PIPAdditionModal

--- src/components/Home/PIPAdditionModal.js
+++ src/components/Home/PIPAdditionModal.js
@@ -7,6 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { FolderOpenOutlined } from '@material-ui/icons';
+
 import {
 	Grid,
 	OutlinedInput,
@@ -18,40 +19,52 @@ import {
 	FormHelperText,
 	Typography,
 } from '@material-ui/core';
+import { isNotEmpty, SlideDialogTransition } from '../../commons/helpers';
+import { useForm, Controller } from 'react-hook-form';
 
 const { ipcRenderer } = window.require('electron');
 
-export default function PIPAdditionModal({ isOpen, handleClose }) {
-	const [pipName, setPIPName] = useState('');
-	const [pipPath, setPipPath] = useState('');
-
-	const [pipNameError, setPIPNameError] = useState('');
-	const [pipPathError, setPipPathError] = useState('');
-
+export default function PIPAdditionModal({ open, handleClose }) {
 	const pickPIP = ev => {
 		ipcRenderer.invoke('PIP_FILE_DIALOG');
 	};
 
-	const handleSubmit = ev => {
-		ipcRenderer.invoke('PIP_ADDITION', pipName, pipPath);
-	};
+	const {
+		register,
+		setValue,
+		setError,
+		errors,
+		handleSubmit,
+		clearErrors,
+		reset,
+		watch,
+		control,
+	} = useForm({
+		mode: 'onSubmit',
+		reValidateMode: 'onSubmit',
+		defaultValues: {
+			pipName: '',
+			pipPath: '',
+		},
+	});
 
-	const handlePIPNameChange = ev => {
-		setPIPName(ev.target.value);
+	const watchPipPath = watch('pipPath');
+
+	const onSubmit = formData => {
+		ipcRenderer.invoke('PIP_ADDITION', formData.pipName, formData.pipPath);
 	};
 
 	useEffect(() => {
 		ipcRenderer.on(
 			'PIP_FILE_DIALOG_RESULTS',
 			function (ev, pipPath, dialogCancelled) {
-				setPipPath(pipPath);
+				setValue('pipPath', pipPath);
 
-				// If the PIP picker dialog is cancelled
+				// if the PIP picker dialog is cancelled
 				if (dialogCancelled) {
-					setPipPathError('Please select a PIP path');
-					return;
+					setError('pipPath', 'Please select a PIP path');
 				} else {
-					setPipPathError('');
+					clearErrors('pipPath');
 				}
 			},
 		);
@@ -61,58 +74,84 @@ export default function PIPAdditionModal({ isOpen, handleClose }) {
 			function (ev, isPIPAdded, pipNameError, pipPathError) {
 				if (isPIPAdded) {
 					// Close the modal
-					setPIPName('');
-					setPipPath('');
 					handleClose();
+					reset();
 				} else {
-					setPIPNameError(pipNameError);
-					setPipPathError(pipPathError);
+					if (pipNameError.length) {
+						setError('pipName', {
+							type: 'manual',
+							message: pipNameError,
+						});
+					} else {
+						clearErrors('pipName');
+					}
+
+					if (pipPathError.length) {
+						setError('pipPath', {
+							type: 'manual',
+							message: pipPathError,
+						});
+					} else {
+						clearErrors('pipPath');
+					}
 				}
 			},
 		);
 	}, []);
 
 	return (
-		<div>
-			<Dialog
-				keepMounted={true}
-				disableBackdropClick={true}
-				open={isOpen}
-				fullWidth={true}
-				onClose={handleClose}
-			>
+		<Dialog
+			style={{ zIndex: 1500 }}
+			keepMounted={true}
+			disableBackdropClick={true}
+			open={open}
+			fullWidth={true}
+			onClose={handleClose}
+			TransitionComponent={SlideDialogTransition}
+		>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<DialogTitle>
 					Add a PIP path
 					<Typography variant="subtitle1" color="textSecondary">
 						Add a PIP path to PyManager
 					</Typography>
 				</DialogTitle>
+
 				<DialogContent>
 					<TextField
-						onKeyUp={handlePIPNameChange}
 						label="PIP name"
-						error={Boolean(pipNameError)}
-						helperText={pipNameError}
+						name="pipName"
+						autoFocus
+						error={isNotEmpty(errors.pipName)}
+						helperText={errors.pipName && errors.pipName.message}
 						type="text"
+						color="secondary"
 						variant="outlined"
 						fullWidth
+						inputRef={register}
 					/>
 
 					<p />
 
-					<FormControl fullWidth={true} variant="outlined">
+					<FormControl
+						fullWidth={true}
+						color="secondary"
+						variant="outlined"
+					>
 						<InputLabel
-							error={pipPathError}
+							error={isNotEmpty(errors.pipPath)}
 							htmlFor="pip-input"
+							shrink={true}
 						>
 							PIP path
 						</InputLabel>
 						<OutlinedInput
 							id="pip-input"
-							error={pipPathError}
-							value={pipPath}
+							error={isNotEmpty(errors.pipPath)}
 							readOnly={true}
-							labelWidth={65}
+							labelWidth={57}
+							inputRef={register}
+							name="pipPath"
 							endAdornment={
 								<InputAdornment position="end">
 									<IconButton onClick={pickPIP} edge="end">
@@ -121,25 +160,29 @@ export default function PIPAdditionModal({ isOpen, handleClose }) {
 								</InputAdornment>
 							}
 						/>
-						<FormHelperText error={pipPathError}>
-							{pipPathError}
+						<FormHelperText
+							color="secondary"
+							error={isNotEmpty(errors.pipPath)}
+						>
+							{errors.pipPath && errors.pipPath.message}
 						</FormHelperText>
 					</FormControl>
-					<p/>
+					<p />
 				</DialogContent>
 				<DialogActions>
 					<Button onClick={handleClose} color="secondary">
 						Cancel
 					</Button>
 					<Button
-						onClick={handleSubmit}
+						// onClick={() => handleSubmit(onSubmit)}
+						type="submit"
 						color="secondary"
 						variant="contained"
 					>
 						Add PIP
 					</Button>
 				</DialogActions>
-			</Dialog>
-		</div>
+			</form>
+		</Dialog>
 	);
 }
