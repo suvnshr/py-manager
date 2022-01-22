48b6f311d7b053b0bef8c2de7347a6f29418e544
suvansh-rana <36293610+suvansh-rana@users.noreply.github.com>
2022-01-22 13:58:49 +0530

    Fixed version select in install confirm dialog

--- src/InstallPackages/InstallConfirmPackageListItem.js
+++ src/InstallPackages/InstallConfirmPackageListItem.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import axios from 'axios';
-
 import {
 	ListItem,
 	ListItemIcon,
@@ -80,10 +78,11 @@ export default function InstallConfirmPackageListItem({
 				{loading ? (
 					loader
 				) : (
-					<FormControl>
+					<FormControl variant="standard">
 						<InputLabel>Version</InputLabel>
 
 						<Select
+							
 							value={currentVersion}
 							onChange={handleVersionChange}
 						>
