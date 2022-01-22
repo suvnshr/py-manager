b5be5b6fdfa587c2cccfd4a8de9114bbb0c2cc9c
suvansh-rana <36293610+suvansh-rana@users.noreply.github.com>
2022-01-22 13:55:39 +0530

    Changed theme colors and changed font

--- src/commons/theme.js
+++ src/commons/theme.js
@@ -1,4 +1,5 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import {blue} from "@mui/material/colors";
 
 export default responsiveFontSizes(
 	createTheme({
@@ -6,8 +7,8 @@ export default responsiveFontSizes(
 			primary: {
 				// 306998
 				// #4B8BBE
-				// main: '#6ba6d6',
-				main: '#4B8BBE',
+				main: '#6ba6d6',
+				// main: '#4B8BBE',
 			},
 			secondary: {
 				main: '#FFD43B',
@@ -15,7 +16,22 @@ export default responsiveFontSizes(
 			mode: 'dark',
 		},
 		typography: {
-			fontFamily: "'Source Sans Pro', Arial, sans-serif",
+			fontFamily: "'DM Sans', sans-serif",
+			h1: {
+				fontFamily: "'Space Mono', monospace",
+			},
+			h2: {
+				fontFamily: "'Space Mono', monospace",
+			},
+			h3: {
+				fontFamily: "'Space Mono', monospace",
+			},
+			h4: {
+				fontFamily: "'Space Mono', monospace",
+			},
+			h5: {
+				fontFamily: "'Space Mono', monospace",
+			},
 		},
 	}),
 );
